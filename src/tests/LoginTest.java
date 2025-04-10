efa124166502a4b0eca83652bae739f07a441fe7
Eba Khalil <ebaqadomi7@gmail.com>
2025-04-10 23:44:46 +0300

    update code

--- src/tests/LoginTest.java
+++ src/tests/LoginTest.java
@@ -1,11 +1,18 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.LoginPage;
 import utils.ExcelReader;
 import java.io.IOException;
+import java.time.Duration;
 
 public class LoginTest extends BaseTest {
 
@@ -31,18 +38,28 @@ public class LoginTest extends BaseTest {
             expectedMessage = "Error: Please enter Email Address in a valid format. | Error: Please enter Current Password.";
         } else if (email.equals("ebakhalil7@gmail.com") && password.equals("123456789Eba@@")) {
             expectedMessage = "You May Also Like"; 
+
+            Actions actions = new Actions(driver);
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(1));
+            WebElement icon = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("(//span[@class='c-button__inner t-header__inner-button'])[1]")));
+            actions.moveToElement(icon).perform();
+            
+            actions.moveToElement(icon).pause(Duration.ofSeconds(3)).perform();
+
+         WebElement myAccount = wait.until(ExpectedConditions.visibilityOfElementLocated(
+             By.xpath("//div[@class='my-account' and text()='My Account']")));
+         Assert.assertTrue(myAccount.isDisplayed(), "My Account option not displayed after hover.");
+
         } else {
-            expectedMessage = "Email/Password you entered is not correct. Please try again.";   
+            expectedMessage = "Email/Password you entered is not correct. Please try again."; 
         }
 
-
         Assert.assertEquals(actualMessage, expectedMessage, "The displayed message does not match the expected result.");
-      
     }
+	private void WebDriverWait(WebDriver driver, Duration ofSeconds) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
-
-
-
-
-
-
